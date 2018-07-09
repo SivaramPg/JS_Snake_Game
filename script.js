f22b810dedccb37ed829d86b6518cb0d027cf705
SivaramPg <sivaramp95@gmail.com>
2018-07-10 02:46:30 +0530

    initial commit with barebones elements with basic styling.

---
+++ script.js
@@ -0,0 +1,12 @@
+const myCanvas = document.getElementById("myCanvas");
+const ctx = myCanvas.getContext("2d");
+
+// Create gradient
+const  grd = ctx.createLinearGradient(0, 0, 200, 0);
+grd.addColorStop(0, "red");
+grd.addColorStop(1, "white");
+
+// Fill with gradient
+ctx.fillStyle = grd;
+ctx.fillRect(10, 10, 150, 80);
+
