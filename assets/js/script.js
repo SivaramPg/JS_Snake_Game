309143df664313af0a501e986aca6efb58d3a832
ojaswa1942 <ojaswa1942@gmail.com>
2018-07-11 03:19:12 +0530

    Integrated Play again and restructured

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const body = document.querySelector("body");
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
-let gameOver = false;
 
 // ** For resizing the canvas depending on the window size.**
 const setDisplay=()=>{
@@ -13,66 +12,62 @@ const setDisplay=()=>{
 window.addEventListener('load', setDisplay);
 window.addEventListener('resize', setDisplay);
 
-// creating a box of fixed dimension and color that will be added to the center as the snake and subsequently added to it as it eats the apples.
-// variable values can be refactored directly into the code if need be 
 const generateSnake = () => {
-
+	const initialSnakeWidth = 30;
+	const initialSnakeHeight = 30;
+	const xPos = (window.innerWidth - initialSnakeWidth) / 2 ;
+	const yPos = (window.innerHeight - initialSnakeHeight) / 2;
+	ctx.fillStyle = "#FF0";
+	//creating snake of length 1 in the middle.
+	ctx.fillRect(xPos, yPos, initialSnakeWidth, initialSnakeHeight);
 }
-//creating snake of length 1 in the middle.
-let initialSnakeWidth = 25;
-let initialSnakeHeight = 25;
-let xPos = (window.innerWidth - initialSnakeWidth) / 2 ;
-let yPos = (window.innerHeight - initialSnakeHeight) / 2;
-ctx.fillStyle = "#FF0";
-//creating snake of length 1 in the middle.
-ctx.fillRect(xPos, yPos, initialSnakeWidth, initialSnakeHeight);
 
-//Creating an Apple 
-// TO BE PLACED INSIDE A FUNCTION AND TRIGGERED IF SNAKE EATS IT.
 const generateApple = () => {
-  const appleWidth = 30;
-  const appleHeight = 30;
-  let randXPos = Math.floor(Math.random() * (window.innerWidth - appleWidth));
-  let randYPos = Math.floor(Math.random() * (window.innerHeight - appleHeight));
-  ctx.fillStyle = "#FF0F00";
-  ctx.fillRect(randXPos, randYPos, appleWidth, appleHeight);
+	const appleWidth = 30;
+	const appleHeight = 30;
+	let randXPos = Math.floor(Math.random() * (window.innerWidth - appleWidth));
+	let randYPos = Math.floor(Math.random() * (window.innerHeight - appleHeight));
+	ctx.fillStyle = "#FF0F00";
+	ctx.fillRect(randXPos, randYPos, appleWidth, appleHeight);
 }
-generateApple();
+
+//Triggers for initiation and restart
+const play = () => {
+	console.log('Play');
+	generateApple();
+	generateSnake();
+}
+window.addEventListener('load', play);
+
 // listening for arrow keys and escape to end the game.
 document.addEventListener("keydown", (event) => {
-  switch(event) {
-    // Use either which or keyCode, depending on browser support
-    case (event.which === 37 || event.keyCode === 37):
-      const goLeft = () => {
-        
-      };
-      break;
-    case (event.which === 38 || event.keyCode === 38):
-      goUp();
-      break;
-    case (event.which === 39 || event.keyCode === 39):
-      goRight();
-      break;
-    case (event.which === 40 || event.keyCode === 40):
-      goDown();
-      break;
-    case (event.which === 27 || event.keyCode === 27):
-      gameOver = true;
-    default:
-     
-    }
-  }
-);
+	console.log(event.which);
+	var key = event.which || event.keyCode;
+  	switch(key) {
+	    case 37:
+	    	console.log('Left');
+			goLeft();
+			break;
+	    case 38:
+			console.log('Up');
+	      	goUp();
+	      	break;
+	    case 39:
+	    	console.log('Right');
+	      	goRight();
+	      	break;
+	    case 40:
+	    	console.log('Down');
+	      	goDown();
+	      	break;
+	    case 27:
+	      	gameOverRun();
+	      	break;
+   }
+});
 
-//Collision
-const outOfBounds = () => {
-
-}
-
-if (!gameOver) {
-
+//After collision condition and escape
+const gameOverRun = () => {
+	alert('Game Over! Play Again?');
+	play();
 } 
-else {
-  // Ask player if they want to play again if yes then reset gameOver and start main function.
-  playAgain ();
-}
