edd1531aa4d6c81a8724c700c0c0000281ecb202
ojaswa1942 <ojaswa1942@gmail.com>
2018-07-11 01:36:10 +0530

    Modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,33 @@
 const body = document.querySelector("body");
 const canvas = document.getElementById("myCanvas");
-
 const ctx = canvas.getContext("2d");
 
-
 // ** For resizing the canvas depending on the window size.**
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const setDisplay=()=>{
+	const headerHeight = document.querySelector('header').offsetHeight;
+	const footerHeight = document.querySelector('header').offsetHeight;
+	canvas.height = window.innerHeight -headerHeight -footerHeight;
+	canvas.width = window.innerWidth;
+}
+window.addEventListener('load', setDisplay);
+window.addEventListener('resize', setDisplay);
 
 // creating a box of fixed dimension and color that will be added to the center as the snake and subsequently added to it as it eats the apples.
 // variable values can be refactored directly into the code if need be 
-let initialSnakeWidth = 25;
-let initialSnakeHeight = 25;
+const initialSnakeHeight = 30;
+const initialSnakeWidth= 30;
 let xpos = (window.innerWidth - initialSnakeWidth) / 2 ;
 let ypos = (window.innerHeight - initialSnakeHeight) / 2;
 ctx.fillStyle = "#FF0";
-
 //creating snake of length 1 in the middle.
 ctx.fillRect(xpos, ypos, initialSnakeWidth, initialSnakeHeight);
 
 //Creating an Apple 
 // TO BE PLACED INSIDE A FUNCTION AND TRIGGERED IF SNAKE EATS IT.
-let appleWidth = 35;
-let appleHeight = 35;
+const appleHeight= 30;
+const appleWidth= 30;
 let randXPos = Math.floor(Math.random() * (window.innerWidth - appleWidth));
 let randYPos = Math.floor(Math.random() * (window.innerHeight - appleHeight));
 ctx.fillStyle = "#FF0F00";
 ctx.fillRect(randXPos, randYPos, appleWidth, appleHeight);
 
-
-// Page scrolls to canvas element on load... Works fine on Edge and chrome when tab closed and reopened but bugs out on refresh.
-window.addEventListener('load', () => {
-  canvas.scrollIntoView({behavior: "smooth", block: "start"})
-});
